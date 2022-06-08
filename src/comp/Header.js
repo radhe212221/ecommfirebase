640b515aa1a99c17b9c7625952e531dd5b130040
RS <radhe212221@gmail.com>
2022-06-08 21:59:35 +0530

    changed header

--- src/comp/Header.js
+++ src/comp/Header.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
   const [menu, setmenu] = useState(false)
+  const state = useSelector(s => s)
+  const { loggedin, user } = state
   return (
     <>
       <header>
         <h1 onClick={() => setmenu(!menu)}>Ecommerce <i className='fa fa-bars'></i></h1>
-        <div>
-          <button>logout (?)</button>
+        <div className='header-options'>
+          <Link to="/home">home</Link>
+          {!loggedin && <Link to="/login">login</Link>}
+          {!loggedin && <Link to="/signup">signup</Link>}
+
+          {loggedin && <Link to="/profile">profile</Link>}
+          {loggedin && <Link to="/wishlist">wishlist</Link>}
+          {loggedin && <Link to="/cart">cart</Link>}
+          {loggedin && <Link to="/orders">orders</Link>}
         </div>
+        {loggedin && <div>
+          <button>logout ({user?.name})</button>
+        </div>}
       </header>
       <aside className={menu ? "active" : ""}>
-        <div><Link to="/home">home</Link></div>
-        <div><Link to="/profile">profile</Link></div>
-        <div><Link to="/wishlist">wishlist</Link></div>
-        <div><Link to="/login">login</Link></div>
-        <div><Link to="/signup">signup</Link></div>
-        <div><Link to="/cart">cart</Link></div>
-        <div><Link to="/orders">orders</Link></div>
+
       </aside>
     </>
   )
